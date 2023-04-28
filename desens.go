2f9d9d508ec6e0cb5d2b29685d9906621833d294
CNDanielLee <768318841@qq.com>
2023-04-29 02:13:37 +0800

    v1.0.1

--- desens.go
+++ desens.go
@@ -11,11 +11,28 @@ func DesensitizeEmail(source string) string {
 	return reg.ReplaceAllString(source, "$1***@$2")
 }
 
+type PhoneDigit uint8
+
+const (
+	PhoneDigitFour PhoneDigit = iota
+	PhoneDigitSix
+)
+
 // DesensitizePhone 手机号脱敏
-func DesensitizePhone(source string) string {
-	reg, err := regexp.Compile("(\\d{3})\\d*(\\d{2})")
-	if err != nil {
-		return ""
+func DesensitizePhone(source string, digit PhoneDigit) string {
+	switch digit {
+	case PhoneDigitFour:
+		reg, err := regexp.Compile("(\\d{3})\\d*(\\d{4})")
+		if err != nil {
+			return ""
+		}
+		return reg.ReplaceAllString(source, "$1****$2")
+	case PhoneDigitSix:
+		reg, err := regexp.Compile("(\\d{3})\\d*(\\d{2})")
+		if err != nil {
+			return ""
+		}
+		return reg.ReplaceAllString(source, "$1******$2")
 	}
-	return reg.ReplaceAllString(source, "$1******$2")
+
 }
